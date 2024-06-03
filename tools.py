179843dddcc34b18a400a2c999973992977d1448
aryanwalia2003 <141477437+aryanwalia2003@users.noreply.github.com>
2024-06-04 01:22:50 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -2,3 +2,9 @@ from crewai_tools import YoutubeChannelSearchTool
 
 # Initialize the tool with a specific Youtube channel handle to target your search
 yt_tool = YoutubeChannelSearchTool(youtube_channel_handle='@GeoHistory')
+
+
+# or 
+
+# Initialize the tool to search within any Youtube channel's content the agent learns about during its execution
+# tool = YoutubeChannelSearchTool() #if you want to get all content available on youtube 
